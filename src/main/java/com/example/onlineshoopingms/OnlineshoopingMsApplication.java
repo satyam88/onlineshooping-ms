498ea4ae7643bc32cba2fc03d76b5b3e3440fe6f
Satyam Pandey <satyam@gmail.com>
2023-02-25 08:47:33 +0530

    codepush

---
+++ src/main/java/com/example/onlineshoopingms/OnlineshoopingMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshoopingms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshoopingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshoopingMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlineshoopingms/OnlineshoopingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshoopingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshoopingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
