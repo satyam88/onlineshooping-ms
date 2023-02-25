f56d31a6952bd7561fa03285d61b20d98b835478
Satyam Pandey <satyam@gmail.com>
2023-02-25 09:59:55 +0530

    adding java module

---
+++ src/main/java/com/example/onlineshoopingms/kids_cloths.java
@@ -0,0 +1,13 @@
+package com.example.onlineshoopingms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class kids_cloths {
+    @GetMapping("/kids_cloths")
+    public String getData() {
+        return "Please purchase cloths online";
+    }
+}
